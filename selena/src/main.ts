5d86dfedf23f042192ac70aeb5a44157fa78703d
Robbert <robbertbw@gmail.com>
2019-02-19 21:23:11 +0100

    swapped dotenv.config and grpc options import

--- selena/src/main.ts
+++ selena/src/main.ts
@@ -3,16 +3,17 @@ import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/websockets';
 import * as dotenv from 'dotenv';
 import { Config } from './utils/Config';
-import { grpcClientOptions } from './gprc.options';
 
 dotenv.config();
+
+import { grpcClientOptions } from './gprc.options';
 
 async function bootstrap() {
   const fastifyAdapter = new FastifyAdapter({trustProxy: true});
   const app = await NestFactory.create(AppModule, fastifyAdapter, {cors: true});
-  // app.useWebSocketAdapter(new WsAdapter(app));
+  app.useWebSocketAdapter(new WsAdapter(app));
 
-  // app.connectMicroservice(grpcClientOptions);
+  app.connectMicroservice(grpcClientOptions);
 
   await app.listen(Config.getInt('APP_PORT'), Config.getString('APP_HOST'));
   await app.startAllMicroservicesAsync();
