5f18deab2a200d412f8f51c04d499ee85a8604e9
Toon van Strijp <toonvanstrijp@gmail.com>
2019-02-25 11:40:38 +0100

    changed proto structure

--- peter/src/grpc.options.ts
+++ peter/src/grpc.options.ts
@@ -2,17 +2,17 @@ import { Transport, ClientOptions } from '@nestjs/microservices';
 import { join } from 'path';
 import { Config } from './utils/config';
 
-let protoPath = join(__dirname, '..', '..', 'shared', 'protos', 'peter.proto');
-let loaderDir = join(__dirname, '..', '..', 'shared', 'protos', '**/*.proto');
+const protoPath = join(__dirname, '..', '..', 'shared', 'protos', 'peter_service', 'peter_service.proto');
+const loaderDir = join(__dirname, '..', '..', 'shared', 'protos', '**/*.proto');
 
 export const grpcClientOptions: ClientOptions = {
   transport: Transport.GRPC,
   options: {
     url: `${Config.getString('APP_HOST')}:${Config.getString('GRPC_PORT')}`,
     package: 'sono',
-    protoPath: protoPath,
+    protoPath,
     loader: {
       includeDirs: [loaderDir],
-    }
+    },
   },
 };
